0e3ea3005def5a9852e80a36d60ded2999967a9c
MrDlop <1112.mimil@gmail.com>
2022-08-20 21:55:40 +0500

    create chanel update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ intent = disnake.Intents.all()
 bot = commands.Bot(command_prefix=settings_ds['prefix'], intents=intent)
 a = dict()
 
+
 @bot.command()
 async def test(ctx):
     """
@@ -22,19 +23,31 @@ async def afk(ctx, time: int):
 
 
 @bot.command()
-async def cc(ctx, name):
+async def cc(ctx, name, _type_="common"):
     """
-    Create user channel
+    /create_channel <Channel Name> <>
     """
-    channel = await ctx.guild.create_voice_channel(name)
-    if not (ctx.message.author.voice is None):
-        await ctx.message.author.move_to(channel)
+    name = name[:100]
+    if str(_type_) == "private":
+        overwrites = {
+            ctx.guild.default_role: disnake.PermissionOverwrite(view_channel=False),
+            ctx.author: disnake.PermissionOverwrite(view_channel=True, manage_channels=True, mute_members=True,
+                                                    deafen_members=True, move_members=True),
+        }
+        channel = await ctx.guild.create_voice_channel(str(name), overwrites=overwrites)
     else:
-        await ctx.send(f'Move to some voice channel')
+        channel = await ctx.guild.create_voice_channel(str(name))
+    if ctx.author.voice is None:
+        await ctx.send(f'{ctx.author.mention}, так как вы не находитесь ни в одном голосовом канале, '
+                       f'я не могу самостоятельно переместить вас в созданный канал. Если в канала под названием "{str(name)}" никто не зайдёт, '
+                       f'то он будет удален через 10 секунд.')
         await asyncio.sleep(10)
         if len(channel.members) == 0:
             await channel.delete()
-        return
+            return
+    else:
+        await ctx.send(f'It works.')
+        await ctx.message.author.move_to(channel)
 
     def check(a, b, c):
         return len(channel.members) == 0
@@ -47,7 +60,7 @@ async def cc(ctx, name):
 async def on_presence_update(before, after):
     channel = before.guild.get_channel(1010499309386608680)
     if disnake.Status.idle == after.status:
-        await asyncio.sleep(a[before.id])
+        await asyncio.sleep(a[before.id] if before.id in a else 60)
         if disnake.Status.idle == after.status:
             await before.move_to(channel, reason=None)
 
